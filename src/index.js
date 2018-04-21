48a090ccf7b1d9bf5913ee883a4a100d90636d79
rupert-ong <rupert.ong@gmail.com>
2018-04-20 21:25:42 -0400

    Edit entry file for React UI

--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import todoApp from './reducers';
-
-import {
-  addTodo,
-  toggleTodo,
-  setVisibilityFilter,
-  VisibilityFilters
-} from './actions';
+import App from './components/App';
 
 const store = createStore(todoApp);
 
-console.log('Initial state:');
-console.log(store.getState());
-
-// Every time the state changes, log it
-// Note that subscribe() returns a function for unregistering the listener
-const unsubscribe = store.subscribe(() => console.log(store.getState()));
-
-// Dispatch some actions
-console.log('Add Todo Items:');
-store.dispatch(addTodo('Learn about actions'));
-store.dispatch(addTodo('Learn about reducers'));
-store.dispatch(addTodo('Learn about stores'));
-console.log('Toggle Todo Items:');
-store.dispatch(toggleTodo(0));
-store.dispatch(toggleTodo(1));
-console.log('Change visibilityFilter:');
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
-
-// Stop listening to state updates
-unsubscribe();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
